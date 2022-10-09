12a6be2042ba7dc9793c770f44db4d2f45a2f0ec
willsongs <90972685+willsongs@users.noreply.github.com>
2022-10-09 15:59:53 +0530

    Update entertainment.py
    Added .strip within ""

--- entertainment.py
+++ entertainment.py
@@ -30,7 +30,7 @@ def done(message):
     c_id = spltarray[1]
     try:
         markup = types.ReplyKeyboardMarkup(row_width=2)
-        bot.send_message(c_id, f'The movie has been added to the database 😊\n You can retry now\n try saying"```{mv_name}```"', parse_mode = 'MarkdownV2', reply_markup=markup)
+        bot.send_message(c_id, f'The movie has been added to the database 😊\n You can retry now\n try saying"```{mv_name.strip()}```"', parse_mode = 'MarkdownV2', reply_markup=markup)
     except Exception:
         bot.reply_to(message, 'oooops')
 
