75ef2de5f4eb0a97b490495cb73a8bb1b511083a
willsongs <90972685+willsongs@users.noreply.github.com>
2022-11-12 17:03:53 +0530

    Update entertainment.py

--- entertainment.py
+++ entertainment.py
@@ -80,7 +80,7 @@ def name(message):
                 markup = telebot.types.InlineKeyboardMarkup(row_width=1)
                 btn1 = telebot.types.InlineKeyboardButton('Watch', url= watch_link, callback_data="click")
                 btn2 = telebot.types.InlineKeyboardButton('alternate link', url= watch_link1, callback_data="click")
-                markup.add(btn1)
+                markup.add(btn1, btn2)
                 bot.send_photo(message.chat.id, img,f"<b>TITLE:</b> <i>{name}</i>\n"
                                                     f"\n<b>SIZE:</b> <i>{file_size}</i>\n", parse_mode = 'html',reply_markup = markup)
 
