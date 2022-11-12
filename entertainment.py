2c902c291ce98a6f2045a5fa1073bd04cb60c506
willsongs <90972685+willsongs@users.noreply.github.com>
2022-11-12 17:05:00 +0530

    Update entertainment.py

--- entertainment.py
+++ entertainment.py
@@ -76,7 +76,7 @@ def name(message):
                 s = round(size / p, 2)
                 file_size = "%s %s" % (s, size_name[i])
                 watch_link = f"https://dood.wf/d/{code}"
-                watch_link = f"https://dood.re/d/{code}"
+                watch_link1 = f"https://dood.re/d/{code}"
                 markup = telebot.types.InlineKeyboardMarkup(row_width=1)
                 btn1 = telebot.types.InlineKeyboardButton('Watch', url= watch_link, callback_data="click")
                 btn2 = telebot.types.InlineKeyboardButton('alternate link', url= watch_link1, callback_data="click")
