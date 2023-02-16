0eeb17a659e276ad4666a6b97c33b5d95d4caccc
willsongs <90972685+willsongs@users.noreply.github.com>
2023-02-16 10:21:44 +0530

    Update entertainment.py
    i commented the part where it calls the function.

--- entertainment.py
+++ entertainment.py
@@ -96,11 +96,11 @@ def name(message):
             threads = []
             for i in range(n):
                 try:
-                    t = threading.Thread(target=fetch_size(), args=(code,))
-                    t.start()
-                    threads.append(t)
+#                     t = threading.Thread(target=fetch_size(), args=(code,))
+#                     t.start()
+#                     threads.append(t)
 
-                    file_size = threads[i]
+#                     file_size = threads[i]
                     
                     code = parse_json['result'][i]['file_code']
                     img = parse_json['result'][i]['splash_img']
